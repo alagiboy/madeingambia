d709892607a68d55080f069980fa54fa2e315cca
alagiboy <alagiboy@gmail.com>
2017-04-25 20:33:11 -0500

    updated company list - added mig logo

--- data.js
+++ data.js
@@ -1,40 +1,78 @@
 //data.js
 const data = [
   {
-    name  : 'InSIST 1',
-    url :  'http://www.insistglobal.com',
-    email_address: 'info@insistglobal.com',
-    contact : '+220 4390028',
+    name  : 'Taf Gambia',
+    url :  'http://www.tafgambia.com/',
+    email_address: 'info@tafgambia.com',
+    contact : '+220 4123456',
     address :'Kairaba Avenue',
-    logo  : 'http://placehold.it/350x150',
+    logo  : 'img/companies/mig-main.png',
     ranking : 1
   },
+
   {
-    name  : 'InSIST 2',
-    url :  'http://www.insistglobal.com',
-    email_address: 'info@insistglobal.com',
-    contact : '+220 4390028',
+    name  : 'Global Properties',
+    url :  'http://www.globalpropertiesafrica.com',
+    email_address: 'info@globalpropertiesafrica.com',
+    contact : '+220 4123456',
     address :'Kairaba Avenue',
-    logo  : 'http://placehold.it/350x150',
+    logo  : 'img/companies/mig-black.png',
     ranking : 2
   },
   {
-    name  : 'InSIST 4',
+    name  : 'InSIST Global',
     url :  'http://www.insistglobal.com',
     email_address: 'info@insistglobal.com',
     contact : '+220 4390028',
     address :'Kairaba Avenue',
-    logo  : 'http://placehold.it/350x150',
+    logo  : 'img/companies/mig-sub.png',
     ranking : 4
   },
   {
-    name  : 'InSIST 6',
+    name  : 'KMF Technology',
+    url :  'http://www.kmf.gm',
+    email_address: 'info@kmf.gm',
+    contact : '+220 4123456',
+    address :'Kairaba Avenue',
+    logo  : 'img/companies/mig-sub-black.png',
+    ranking : 3
+  },
+
+  {
+    name  : 'Global Properties',
+    url :  'http://www.globalpropertiesafrica.com',
+    email_address: 'info@globalpropertiesafrica.com',
+    contact : '+220 4123456',
+    address :'Kairaba Avenue',
+    logo  : 'img/companies/mig-black.png',
+    ranking : 6
+  },
+  {
+    name  : 'Taf Gambia',
+    url :  'http://www.tafgambia.com/',
+    email_address: 'info@tafgambia.com',
+    contact : '+220 4123456',
+    address :'Kairaba Avenue',
+    logo  : 'img/companies/mig-main.png',
+    ranking : 5
+  },
+  {
+    name  : 'KMF Technology',
+    url :  'http://www.kmf.gm',
+    email_address: 'info@kmf.gm',
+    contact : '+220 4123456',
+    address :'Kairaba Avenue',
+    logo  : 'img/companies/mig-sub-black.png',
+    ranking : 8
+  },
+  {
+    name  : 'InSIST Global',
     url :  'http://www.insistglobal.com',
     email_address: 'info@insistglobal.com',
     contact : '+220 4390028',
     address :'Kairaba Avenue',
-    logo  :'http://placehold.it/350x150', //'../public/img/company-logo-placeholder.png',
-    ranking : 6
+    logo  : 'img/companies/mig-sub.png',
+    ranking : 7
   }
 ]
 module.exports = data;
